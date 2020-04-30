fda9bf04da9487e0a535ecd1b6da30e6aadc26d2
Francesco Bianco <info.francescobianco@gmail.com>
2020-04-30 12:59:26 +0200

    Update at gio 30 apr 2020, 12:59:26, CEST

--- update.sh
+++ update.sh
@@ -15,18 +15,20 @@ git push
 for version in "${!versions[@]}"; do
     git checkout -B "${version}" "4d2e80d4c82a84fecb6293b9df8589e04f08d9db"
     git clean -fdx
-    echo "${version} -> ${versions[$version]}"
-    echo ${versions[$version]} > SOURCE
-    curl -o vtiger.tar.gz -L "${download_files}${versions[$version]}"
+    echo "====[ ${version} ]===="
+    echo "Dowload: ${download_files}${versions[$version]}"
+    curl -o vtiger.tar.gz -sL "${download_files}${versions[$version]}"
     tar -xzf vtiger.tar.gz
     cd vtigercrm
-    tar -cvzf files.tar.gz .
+    tar -czf files.tar.gz .
     cd ..
     tar -xzf vtigercrm/files.tar.gz
-    rm -fr vtigercrm vtiger.tar.gz
+    rm -fr vtigercrm/
+    rm vtiger.tar.gz
     git add .
     git commit -am "Update version ${version}"
     git pull --no-edit
+    git tag "${version}"
     git push --set-upstream origin "${version}"
 done
 
