e8f1c9afd63c220439d5eacc21b7f519705cc73b
Francesco Bianco <info.francescobianco@gmail.com>
2020-04-30 12:05:19 +0200

    Update at gio 30 apr 2020, 12:05:19, CEST

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source versions.sh
+
+git add .
+git commit -am "Update at $(date)"
+git push
+
+for version in "${!versions[@]}"; do
+    echo ${version} -> ${versions[$version]}
+done
