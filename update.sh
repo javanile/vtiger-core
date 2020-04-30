25a672750dc8cd30e4e443ada3dd0c9c9e25b0b7
Francesco Bianco <info.francescobianco@gmail.com>
2020-04-30 12:10:14 +0200

    Update at gio 30 apr 2020, 12:10:14, CEST

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ source versions.sh
 git config credential.helper cache
 git config credential.helper 'cache --timeout=3600'
 
+date > RELEASE
+
 git add .
 git commit -am "Update at $(date)"
 git push
