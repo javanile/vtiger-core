528708a54fe9a514f24a2a1157e37160eee13a42
Francesco Bianco <info.francescobianco@gmail.com>
2020-05-01 10:26:27 +0200

    ad

--- update.sh
+++ update.sh
@@ -3,41 +3,40 @@ set -e
 
 source versions.sh
 
-git config credential.helper cache
-git config credential.helper 'cache --timeout=3600'
-
-date > RELEASE
-
-git add .
-git commit -am "Update at $(date)"
-git push
+mkdir -p cache
+git clone https://github.com/javanile/vtiger-core.git build && true
 
 for version in "${!versions[@]}"; do
-    echo "======[ ${version} ]======"
-    git checkout -B "v${version}" "2e7db593df0a9b755489c36db8fb6c706252bf3d"
-    git clean -fdx
-    echo "-> Downloading..."
-    curl -o vtiger.tar.gz -L# "${download_files}${versions[$version]}"
-    echo "-> Extracting..."
-    tar -xzf vtiger.tar.gz
-    cd vtigercrm
-    tar -czf ../files.tar.gz .
-    cd ..
-    tar -xzf files.tar.gz
-    echo "-> Cleaning..."
-    rm -fr vtigercrm/
-    rm vtiger.tar.gz
-    rm files.tar.gz
-    echo "-> Committing..."
-    git add .
-    git commit -am "Update version ${version}"
-    git pull --no-edit origin blank
-    echo "-> Pushing..."
-    git push --set-upstream origin "v${version}"
-    echo "-> Tagging..."
-    git tag -fa "${version}" -m "${version}"
-    git push origin --tags
+  echo "======[ ${version} ]======"
+  echo "-> Downloading..."
+  if [[ ! -f "cache/${version}.tar.gz" ]]; then
+    curl -o "cache/${version}.tar.gz" -L# "${download_files}${versions[$version]}"
+  fi
+  cd build
+  git config credential.helper cache
+  git config credential.helper 'cache --timeout=3600'
+  git checkout -B "v${version}" "2e7db593df0a9b755489c36db8fb6c706252bf3d"
+  git clean -fdx
+  echo "-> Extracting..."
+  tar -xzf "../cache/${version}.tar.gz"
+  cd vtigercrm
+  tar -czf ../files.tar.gz .
+  cd ..
+  tar -xzf files.tar.gz
+  echo "-> Cleaning..."
+  rm -fr vtigercrm/
+  rm files.tar.gz
+  echo "-> Updating..."
+  cp ../composer.json.tpl ./composer.json
+  sed -e 's!%VERSION%!'"${version}"'!' -ri composer.json
+  echo "-> Committing..."
+  git add .
+  git commit -am "Update version ${version}"
+  echo "-> Pushing..."
+  git pull --no-edit origin "v${version}" && true
+  git push --set-upstream origin "v${version}"
+  echo "-> Tagging..."
+  git tag -fa "${version}" -m "${version}"
+  git push origin --tags -f
+  cd ..
 done
-
-git checkout master
-git clean -fdx
--- versions.sh
+++ versions.sh
@@ -5,9 +5,9 @@ download_files=http://sourceforge.net/projects/vtigercrm/files/
 
 declare -A versions
 versions=(
-     ["7.2.0"]=vtiger%20CRM%207.2.0/Core%20Product/vtigercrm7.2.0.tar.gz
      ["7.1.0"]=vtiger%20CRM%207.1.0/Core%20Product/vtigercrm7.1.0.tar.gz
 )
+#     ["7.2.0"]=vtiger%20CRM%207.2.0/Core%20Product/vtigercrm7.2.0.tar.gz
 #     ["7.1.0-RC"]=vtiger%20CRM%207.1.0%20RC/Core%20Product/vtigercrm7.1.0rc.tar.gz
 #     ["7.0.1"]=vtiger%20CRM%207.0.1/Core%20Product/vtigercrm7.0.1.tar.gz
 #     ["7.0.0"]=vtiger%20CRM%207.0/Core%20Product/vtigercrm7.0.0.tar.gz
